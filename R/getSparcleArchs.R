20c4fd7996a29b7f65ece0bb6ce5fa22393c055c
Jose V. Die <jodiera@upv.es>
2020-07-26 12:35:23 +0200

    pass along several ids

--- R/getSparcleArchs.R
+++ R/getSparcleArchs.R
@@ -17,9 +17,14 @@
 getSparcleArchs <-
 function(CD){
 
-  id = entrez_search(db = "cdd", term = paste0(CD,"[ALL]"))
-  cd = entrez_summary(db = "cdd", id = id$ids)
-  sparcle = entrez_link(dbfrom = "cdd", db = "sparcle", id = cd$uid)
-  sparcle$links$cdd_sparcle
+    cdd_sparcle = c()
+    id = entrez_search(db = "cdd", term = paste0(CD,"[ALL]"))
+    for(i in seq_along(id$ids)) {
+      cd = entrez_summary(db = "cdd", id = id$ids[i])
+      sparcle = entrez_link(dbfrom = "cdd", db = "sparcle", id = cd$uid)
+      cdd_sparcle = c(cdd_sparcle, sparcle$links$cdd_sparcle)
+    }
+    
+    cdd_sparcle
 
   }
